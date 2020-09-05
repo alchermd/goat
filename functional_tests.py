95bfc17f25613b31f7cd7c680e44f35115aceef2
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-09-05 10:58:15 +0800

    Create helper method for checking if a row exists in the table

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_list_in_table(self, row_text):
+        table = self.browser.find_element_by_id("list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Micah has heard about a cool new online to-do app. She goes to check out its homepage.
         self.browser.get("http://localhost:8000")
@@ -36,10 +41,8 @@ class NewVisitorTest(unittest.TestCase):
         # "1. Buy watercolor paint" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_list_in_table("1. Buy watercolor paint")
 
-        table = self.browser.find_element_by_id("list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1. Buy watercolor paint", [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She enters "Add water to the watercolor paint".
         inputbox = self.browser.find_element_by_id("new_item")
@@ -48,10 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list.
-        table = self.browser.find_element_by_id("list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1. Buy watercolor paint", [row.text for row in rows])
-        self.assertIn("2. Add water to the watercolor paint", [row.text for row in rows])
+        self.check_for_row_list_in_table("1. Buy watercolor paint")
+        self.check_for_row_list_in_table("2. Add water to the watercolor paint")
 
         # Micah wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for
         # her -- there is some explanatory text to that effect.
