b7254fd2632f87d3d984b122ba99d5d2a1a6c4c2
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-17 17:01:29 +0800

    Delete lists directory

--- lists/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list_list, name='list_list'),
-    path('<list_id>/', views.list_detail, name='list_detail'),
-]
